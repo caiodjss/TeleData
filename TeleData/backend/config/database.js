fe75f763920e3b66312444cfbf0df140cbca7670
Caio de Jesus <122991872+caiodjss@users.noreply.github.com>
2025-09-08 13:01:53 -0300

    Update database.js

--- TeleData/backend/config/database.js
+++ TeleData/backend/config/database.js
@@ -1,10 +1,9 @@
-// Importa Sequelize
 const { Sequelize } = require("sequelize");
 
-// Importa dotenv para ler as variáveis do .env
+// Para ler as variáveis do .env
 require("dotenv").config();
 
-// Cria a instância do Sequelize usando as variáveis de ambiente
+// Instância do Sequelize usando as variáveis de ambiente
 const sequelize = new Sequelize(
   process.env.DB_NAME,      // Nome do banco
   process.env.DB_USER,      // Usuário
@@ -12,9 +11,8 @@ const sequelize = new Sequelize(
   {
     host: process.env.DB_HOST,
     dialect: process.env.DB_DIALECT || "mysql",
-    logging: false, // opcional: desativa logs SQL
+    logging: false, // Desativa logs SQL
   }
 );
 
-// Exporta a instância para ser usada em outros arquivos
 module.exports = sequelize;
