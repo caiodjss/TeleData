30a50fc6d9179ecaea9b85af80797b01fa9cdbd3
Caio de Jesus <122991872+caiodjss@users.noreply.github.com>
2025-09-11 08:06:49 -0300

    Update User.js
    Em breve a opção de recuperar senha.

--- TeleData/backend/models/User.js
+++ TeleData/backend/models/User.js
@@ -1,54 +1,24 @@
 const { DataTypes } = require("sequelize");
 const sequelize = require("../config/database");
 
-const User = sequelize.define(
-  "User",
-  {
-    user_id: {
-      type: DataTypes.BIGINT,
-      autoIncrement: true,
-      primaryKey: true,
-    },
-    full_name: {
-      type: DataTypes.STRING(255),
-      allowNull: false,
-    },
-    email: {
-      type: DataTypes.STRING(255),
-      allowNull: false,
-      unique: true,
-    },
-    password_hash: {
-      type: DataTypes.STRING(255),
-      allowNull: false,
-    },
-    profile_headline: {
-      type: DataTypes.STRING(255),
-      allowNull: true,
-    },
-    biography: {
-      type: DataTypes.TEXT,
-      allowNull: true,
-    },
-    profile_image_url: {
-      type: DataTypes.STRING(2048),
-      allowNull: true,
-    },
-    created_at: {
-      type: DataTypes.DATE,
-      allowNull: false,
-      defaultValue: DataTypes.NOW,
-    },
-    updated_at: {
-      type: DataTypes.DATE,
-      allowNull: false,
-      defaultValue: DataTypes.NOW,
-    },
+const User = sequelize.define("User", {
+  user_id: {
+    type: DataTypes.BIGINT,
+    autoIncrement: true,
+    primaryKey: true,
   },
-  {
-    tableName: "Users",
-    timestamps: false, //created_at e updated_at manualmente
-  }
-);
+  email: {
+    type: DataTypes.STRING(255),
+    allowNull: false,
+    unique: true,
+  },
+  password_hash: {
+    type: DataTypes.STRING(255),
+    allowNull: false,
+  },
+}, {
+  tableName: "Users",
+  timestamps: false,
+});
 
 module.exports = User;
